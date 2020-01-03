481c318e757bf7d2d17bff0b55b6a941b41f8b67
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-01-03 09:54:22 +0900

    fixed db to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_20_135739) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dones", force: :cascade do |t|
     t.integer "performance_id"
     t.integer "user_id"
